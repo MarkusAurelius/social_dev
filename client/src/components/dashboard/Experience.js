113d2b3a3c26099dd9c8d5d81669acfa5686a606
MarkusAurelius <markus.mummert@googlemail.com>
2020-08-29 10:58:24 +0200

    Fix issue at the dashboard

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -1,24 +1,32 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment';
+import moment from 'moment';
 import { connect } from 'react-redux';
 import { deleteExperience } from '../../actions/profile';
 
 const Experience = ({ experience, deleteExperience }) => {
     const experiences = experience.map(exp => (
-        <tr key={exp._id}>
-            <td>{exp.company}</td>
-            <td className="hide-sm">{exp.title}</td>
-            <td>
-                <Moment format='YYYY/MM/DD'>{exp.format}</Moment> -{''}
-                {exp.to === null ? (
-                        'Now'
-                        ) : (
-                        <Moment format='YYYY/MM/DD'>{exp.to}</Moment>
-                )}
-            </td>
-            <button onClick={() => deleteExperience(exp._id)} className='btn btn-danger'>Delete</button>
-        </tr>
+      <tr key={exp._id}>
+        <td>{exp.company}</td>
+        <td className="hide-sm">{exp.title}</td>
+        <td>
+          <Moment format="YYYY/MM/DD">{moment.utc(exp.from)}</Moment> -{' '}
+          {exp.to === null ? (
+            ' Now'
+          ) : (
+            <Moment format="YYYY/MM/DD">{moment.utc(exp.to)}</Moment>
+          )}
+        </td>
+        <td>
+          <button
+            onClick={() => deleteExperience(exp._id)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
     ));
 
     return (
