fff76b496492da0cf40b64d5aadce08c6eef4dc7
MarkusAurelius <markus.mummert@googlemail.com>
2020-08-31 10:27:16 +0200

    Correct typo that is causing the dashboard display issue with the production build

---
+++ client/build/precache-manifest.69155db1f76e834960bf13ff9d1a7367.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c775263bb65f10cf4875f905221633",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c08990480c17a85126ac",
+    "url": "/static/css/main.6dcbe8d0.chunk.css"
+  },
+  {
+    "revision": "a8543aff7af820f6809c",
+    "url": "/static/js/2.f05bc198.chunk.js"
+  },
+  {
+    "revision": "690d2eb1615747a0721b92de75a0e14c",
+    "url": "/static/js/2.f05bc198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08990480c17a85126ac",
+    "url": "/static/js/main.107f218a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
